b36533b159e04e48eb22d847add52b98f19c13fb
Yuichiro Sakamoto <motya0414@hotmail.co.jp>
2025-09-15 11:47:43 +0900

    軽微な修正

--- test_simple_cost_nofity.sh
+++ test_simple_cost_nofity.sh
@@ -6,5 +6,6 @@ export Region=${Region}
 export PROFILE_NAME=${PROFILE_NAME}
 export SENDER_EMAIL=${SENDER_EMAIL}
 export PROJECT_DATA_PARAMETER_NAME=${PROJECT_DATA_PARAMETER_NAME}
+export RATE_VALUE=${RATE_VALUE}
 
 python lambda/simple_cost_nofity/lambda_function.py
