0701dc39f399c08c66f85c4a65976c29714b00ea
Yuichiro Sakamoto <motya0414@hotmail.co.jp>
2025-09-15 12:27:03 +0900

    ざっくりと値が出るように変更

--- lambda/simple_cost_nofity/lambda_function.py
+++ lambda/simple_cost_nofity/lambda_function.py
@@ -4,6 +4,14 @@ import os
 from datetime import datetime as dt
 from datetime import timezone
 
+from azure.identity import ClientSecretCredential
+from azure.mgmt.costmanagement import CostManagementClient
+from azure.mgmt.costmanagement.models import (
+    QueryAggregation,
+    QueryDataset,
+    QueryDefinition,
+    QueryGrouping,
+)
 from boto3.session import Session
 
 logger = logging.getLogger()
@@ -30,7 +38,7 @@ def get_ssm_parameter():
     return parameter_value
 
 
-def get_cost_and_usage():
+def get_aws_cost_and_usage():
     # aws ce get-cost-and-usage --time-period Start=2025-01-01,End=2025-01-31 --granularity DAILY --metrics "UnblendedCost" --group-by Type=DIMENSION,Key=SERVICE Type=DIMENSION,Key=LINKED_ACCOUNT --profile maina
 
     today = dt.now(timezone.utc)
@@ -56,7 +64,7 @@ def get_cost_and_usage():
     return response
 
 
-def sort_out_cost(cost_datas, project_data):
+def sort_out_aws_cost(cost_datas, project_data):
     DEFAULT_PROJECT = project_data["default_project"]
     _project_data = project_data["project_data"]
     cost_results = {}
@@ -76,16 +84,96 @@ def sort_out_cost(cost_datas, project_data):
             for project_name, cost_result in cost_results.items():
                 if account_id in _project_data[project_name]["AccountID"]:
                     if service in cost_result.keys():
-                        cost_result[service] += usd
+                        cost_result[service] += usd * RATE
                     else:
-                        cost_result[service] = usd
+                        cost_result[service] = usd * RATE
                     project_flag = True
 
             if project_flag is False:
                 if service in cost_results[DEFAULT_PROJECT].keys():
-                    cost_results[DEFAULT_PROJECT][service] += usd
+                    cost_results[DEFAULT_PROJECT][service] += usd * RATE
                 else:
-                    cost_results[DEFAULT_PROJECT][service] = usd
+                    cost_results[DEFAULT_PROJECT][service] = usd * RATE
+
+    return cost_results
+
+
+def get_azure_cost_and_usage(subscription_id: str):
+    # 環境変数から認証情報を取得
+    try:
+        credential = ClientSecretCredential(
+            tenant_id=os.environ["AZ_TENANT_ID"],
+            client_id=os.environ["AZ_CLIENT_ID"],
+            client_secret=os.environ["AZ_CLIENT_SECRET"],
+        )
+    except KeyError:
+        print(
+            "エラー: 必要な環境変数（AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET）が設定されていません。"
+        )
+        return None
+
+    # Cost Management クライアントの初期化
+    cost_management_client = CostManagementClient(credential)
+
+    # クエリのスコープ（対象範囲）を設定
+    scope = f"/subscriptions/{subscription_id}"
+
+    # Cost Management APIに投げるクエリを定義
+    query_definition = QueryDefinition(
+        type="ActualCost",  # 実績コストを取得
+        timeframe="MonthToDate",  # 期間を「当月初日から本日まで」に設定
+        dataset=QueryDataset(
+            granularity="Daily",  # 日単位で集計
+            aggregation={"totalCost": QueryAggregation(name="Cost", function="Sum")},
+            grouping=[
+                QueryGrouping(type="Dimension", name="SubscriptionId"),
+                QueryGrouping(type="Dimension", name="SubscriptionName"),
+                QueryGrouping(type="Dimension", name="ServiceName"),
+            ],
+        ),
+    )
+
+    try:
+        # APIを実行してコスト情報を取得
+        result = cost_management_client.query.usage(scope, query_definition)
+
+        # 結果を整形
+        # columns = [col.name for col in result.columns]
+        data = result.rows
+
+    except Exception as e:
+        print(f"エラーが発生しました: {e}")
+        return None
+
+    return data
+
+
+def sort_out_azure_cost(cost_datas, project_data):
+    DEFAULT_PROJECT = project_data["default_project"]
+    _project_data = project_data["project_data"]
+    cost_results = {}
+    for project_name in _project_data.keys():
+        cost_results[project_name] = {}
+
+    for cost_data in cost_datas:
+        cost_jpy = cost_data[0]
+        sub_id = cost_data[2]
+        service = cost_data[4]
+
+        project_flag = False
+        for project_name, cost_result in cost_results.items():
+            if sub_id in _project_data[project_name]["SubscriptionID"]:
+                if service in cost_result.keys():
+                    cost_result[service] += cost_jpy
+                else:
+                    cost_result[service] = cost_jpy
+                project_flag = True
+
+        if project_flag is False:
+            if service in cost_results[DEFAULT_PROJECT].keys():
+                cost_results[DEFAULT_PROJECT][service] += cost_jpy
+            else:
+                cost_results[DEFAULT_PROJECT][service] = cost_jpy
 
     return cost_results
 
@@ -99,7 +187,7 @@ def create_email_html(sort_cost_data, budget_yen):
         tbody += f"""<tr>
                 <td>{i}</td>
                 <td>{item}</td>
-                <td>{value*RATE:,.2f} 円</td>
+                <td>{value:,.2f} 円</td>
             </tr>"""
         i = i + 1
 
@@ -112,17 +200,17 @@ def create_email_html(sort_cost_data, budget_yen):
     predict_month_cost = cost_per_day * 31
 
     # 予算との差分
-    diff_budget_predict = budget_yen - predict_month_cost * RATE
+    diff_budget_predict = budget_yen - predict_month_cost
 
     cost_report = f"""<div>
             <h2>これまでの利用料金</h2>
-            <p>{total_value*RATE:,.0f} 円</p>
+            <p>{total_value:,.0f} 円</p>
             <h2>今月の料金予測(31日で計算)</h2>
-            <p>{predict_month_cost*RATE:,.0f} 円</p>
+            <p>{predict_month_cost:,.0f} 円</p>
             <h2>予算との差分</h2>
-            <p>予算({budget_yen:,})-予測({predict_month_cost*RATE:,.0f})= {diff_budget_predict:,.0f} 円</p>
+            <p>予算({budget_yen:,})-予測({predict_month_cost:,.0f})= {diff_budget_predict:,.0f} 円</p>
             <h2>予算の利用割合</h2>
-            <p>{round(total_value*RATE*100/budget_yen)} %</p>
+            <p>{round(total_value*100/budget_yen)} %</p>
         </div>
 
         <div>
@@ -182,8 +270,24 @@ def send_email(project, cost_report):
 
 def lambda_handler(event, context):
     project_data = get_ssm_parameter()
-    cost_datas = get_cost_and_usage()["ResultsByTime"]
-    sort_results = sort_out_cost(cost_datas, project_data)
+
+    # AWS
+    aws_cost_datas = get_aws_cost_and_usage()["ResultsByTime"]
+    sort_aws_results = sort_out_aws_cost(aws_cost_datas, project_data)
+
+    # Azure
+    target_subscription_id = os.environ["AZ_SUB_ID"]
+    azure_cost_data = get_azure_cost_and_usage(target_subscription_id)
+    sort_azure_results = sort_out_azure_cost(azure_cost_data, project_data)
+
+    # join
+    sort_results = sort_aws_results
+    for project, sort_result in sort_azure_results.items():
+        if sort_results.get(project, None) is not None:
+            sort_results[project].update(sort_result)
+        else:
+            sort_results[project] = sort_result
+
     for project, sort_result in sort_results.items():
         cost_report = create_email_html(
             sort_result, project_data["project_data"][project]["budget_yen"]
--- test_simple_cost_nofity.sh
+++ test_simple_cost_nofity.sh
@@ -7,5 +7,9 @@ export PROFILE_NAME=${PROFILE_NAME}
 export SENDER_EMAIL=${SENDER_EMAIL}
 export PROJECT_DATA_PARAMETER_NAME=${PROJECT_DATA_PARAMETER_NAME}
 export RATE_VALUE=${RATE_VALUE}
+export AZ_CLIENT_ID=${AZ_CLIENT_ID}
+export AZ_CLIENT_SECRET=${AZ_CLIENT_SECRET}
+export AZ_TENANT_ID=${AZ_TENANT_ID}
+export AZ_SUB_ID=${AZ_SUB_ID}
 
 python lambda/simple_cost_nofity/lambda_function.py
